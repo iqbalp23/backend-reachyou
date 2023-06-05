b1e19cb9074f5ff253a2262318c759a272076497
iqbalp23 <mipiqbalprasetya@gmail.com>
2023-06-05 21:12:16 +0000

    .

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('auth_db','root','',{
-    host: "localhost",
+const db = new Sequelize('auth_db','authdb','qwerty123QAZ',{
+    host: "34.171.140.15",
     dialect: "mysql"
 });
 
